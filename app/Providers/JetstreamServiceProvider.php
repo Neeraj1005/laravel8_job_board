e6d8ffabdcc89b42e237741012fe1b7d5af0ff16
Neeraj1005 <neeraj.tangariya123@gmail.com>
2020-11-05 21:05:27 +0530

    initial job board employee and company login register added

---
+++ app/Providers/JetstreamServiceProvider.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Laravel\Fortify\Fortify;
+use Laravel\Jetstream\Jetstream;
+use Illuminate\Support\Facades\Hash;
+use App\Actions\Jetstream\CreateTeam;
+use App\Actions\Jetstream\DeleteTeam;
+use App\Actions\Jetstream\DeleteUser;
+use Illuminate\Support\ServiceProvider;
+use App\Actions\Jetstream\AddTeamMember;
+use App\Actions\Jetstream\UpdateTeamName;
+
+class JetstreamServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->configurePermissions();
+
+        Jetstream::createTeamsUsing(CreateTeam::class);
+        Jetstream::updateTeamNamesUsing(UpdateTeamName::class);
+        Jetstream::addTeamMembersUsing(AddTeamMember::class);
+        Jetstream::deleteTeamsUsing(DeleteTeam::class);
+        Jetstream::deleteUsersUsing(DeleteUser::class);
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)->where('type', $request->type)->first();
+
+            if ($user &&
+                Hash::check($request->password, $user->password)) {
+                return $user;
+            }
+        });
+    }
+
+    /**
+     * Configure the roles and permissions that are available within the application.
+     *
+     * @return void
+     */
+    protected function configurePermissions()
+    {
+        Jetstream::defaultApiTokenPermissions(['read']);
+
+        Jetstream::role('admin', __('Administrator'), [
+            'create',
+            'read',
+            'update',
+            'delete',
+        ])->description(__('Administrator users can perform any action.'));
+
+        Jetstream::role('editor', __('Editor'), [
+            'read',
+            'create',
+            'update',
+        ])->description(__('Editor users have the ability to read, create, and update.'));
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                    <div class="mt-8 text-2xl">
+                        Welcome to Job Portal Application!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
