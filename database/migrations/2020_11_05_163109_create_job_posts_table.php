61450bd895e4a0844a7f25fd7980dabcf282daff
Neeraj1005 <neeraj.tangariya123@gmail.com>
2020-11-05 22:09:29 +0530

    JobPost model and factory added

---
+++ database/migrations/2020_11_05_163109_create_job_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('user_id');
+            $table->longText('description');
+            $table->boolean('is_active');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_posts');
+    }
+}
